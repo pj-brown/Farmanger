693c8a5611a53df5378e9740799c0fd319763f50
Patrick Brown <pjbrown05@gmail.com>
2020-07-25 19:57:59 -0500

    added placeholder delete/update buttons for crops and starter crop js function file

--- public/js/crop.js
+++ public/js/crop.js
@@ -1,15 +1,29 @@
 $(document).ready(function() {
 
+    // on lick listeners for nav bar buttons
     $(document).on("click", "#add-crop", getCropPage);
     $(document).on("click", "#add-field", getFieldPage);
 
+    // goes to the crop page
     function getCropPage() {
         window.location.href = "../html/crops.html"
     }
 
+    // goes to the field page
     function getFieldPage() {
         window.location.href = "../html/fields.html"
     }
 
+    // // call getCrops() to get crops
+    // getCrops();
 
+    // // function to get crops from api
+    // function getCrops() {
+    //     $.get("/api/crops", renderCropList);
+    // }
+
+    // // function to render crops to table
+    // function renderCropList(crop) {
+    //     let 
+    // }
 });
